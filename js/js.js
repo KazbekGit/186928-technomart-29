aaf22271bf32e9b5cedcd3b617265db18348aafb
Kazbek <Kazbekgit@gmail.com>
2020-11-10 08:34:44 +0300

    Доступная разметка главной страницы

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
-let btns = document.querySelectorAll("[data-modal]");
+let btns = document.querySelectorAll('[data-modal]');
 
 for (let index = 0; index < btns.length; index++) {
-  btns[index].addEventListener("click", function (event) {
+  btns[index].addEventListener('click', function (event) {
     event.preventDefault();
     event.stopPropagation();
 
@@ -9,7 +9,7 @@ for (let index = 0; index < btns.length; index++) {
     const modalTypes = document.querySelectorAll("[data-modal-type='" + type + "']");
 
     modalTypes.forEach(element => {
-        element.classList.toggle("visually-hidden");
+      element.classList.toggle('visually-hidden');
   });
 });
 }
@@ -25,30 +25,29 @@ if (document.querySelector('.index-page')) {
   let serviceListButtons = document.getElementsByClassName('service-list');
   let caruselItem = document.getElementById('carusel-item-id');
 
-
   leftArrowOfSlider.onclick = function (event) {
     event.preventDefault();
     event.stopPropagation();
-    sliderTrack.style.transform = "translateX(0px)";
+    sliderTrack.style.transform = 'translateX(0px)';
     buttonItems[0].classList.add('button-current');
     buttonItems[1].classList.remove('button-current');
   }
 
   rightArrowOfSlider.onclick = function (event) {
     event.preventDefault();
-    sliderTrack.style.transform = "translateX(-620px)";
+    sliderTrack.style.transform = 'translateX(-620px)';
     buttonItems[0].classList.remove('button-current');
     buttonItems[1].classList.add('button-current');
   }
 
   buttonItems[0].onclick = function (event) {
-    sliderTrack.style.transform = "translateX(0px)";
+    sliderTrack.style.transform = 'translateX(0px)';
     buttonItems[0].classList.add('button-current');
     buttonItems[1].classList.remove('button-current');
   }
 
   buttonItems[1].onclick = function (event) {
-    sliderTrack.style.transform = "translateX(-620px)";
+    sliderTrack.style.transform = 'translateX(-620px)';
     buttonItems[0].classList.remove('button-current');
     buttonItems[1].classList.add('button-current');
   }
@@ -61,25 +60,24 @@ if (document.querySelector('.index-page')) {
     serviceListButtons[i].onclick = function () {
         let shiftY = 0;
         switch (i) {
-            case 0: shiftY = caruselItem.offsetHeight / serviceListButtons.length * i;
-                caruselItem.style.transform = 'translateY(' + -shiftY + 'px)';
-                break;
-            case 1: shiftY = caruselItem.offsetHeight / serviceListButtons.length * i;
-                caruselItem.style.transform = 'translateY(' + -shiftY + 'px)';
-                break;
-            case 2: shiftY = caruselItem.offsetHeight / serviceListButtons.length * i;
-                caruselItem.style.transform = 'translateY(' + -shiftY + 'px)';
-                break;
+          case 0: shiftY = caruselItem.offsetHeight / serviceListButtons.length * i;
+              caruselItem.style.transform = 'translateY(' + -shiftY + 'px)';
+              break;
+          case 1: shiftY = caruselItem.offsetHeight / serviceListButtons.length * i;
+              caruselItem.style.transform = 'translateY(' + -shiftY + 'px)';
+              break;
+          case 2: shiftY = caruselItem.offsetHeight / serviceListButtons.length * i;
+              caruselItem.style.transform = 'translateY(' + -shiftY + 'px)';
+              break;
         }
         for (let i = 0; i < serviceListButtons.length; i++) {
-            if (serviceListButtons[i] === this) {
-                this.classList.add('active-list-button');
-                continue;
+          if (serviceListButtons[i] === this) {
+              this.classList.add('active-list-button');
+              continue;
             }
-            serviceListButtons[i].classList.remove('active-list-button');
+          serviceListButtons[i].classList.remove('active-list-button');
         }
 
     }
-
   }
 }
